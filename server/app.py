b46219e05c6402c15e76e8c1669eac89c8e30abe
Animesh Bhasin <animeshbhasin1994@gmail.com>
2021-11-13 19:54:12 -0500

    Read secret keys from config file

--- server/app.py
+++ server/app.py
@@ -1,11 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_httpauth import HTTPBasicAuth
 #from thumbtack_conn import thumbtack_json_to_pandas
-
 import pandas as pd
 import db
-import requests
-import os
+import helper
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -13,7 +11,11 @@ auth = HTTPBasicAuth()
 USER_DATA = {
     "jenna": "smith22"
 }
-db_obj = db.Database()
+
+app.config.from_pyfile('config.py')
+
+database_url = app.config['DATABASE_URL']
+db_obj = db.Database(database_url)
 
 @auth.verify_password
 def verify(username, password):
@@ -55,7 +57,7 @@ APP_SECRET='6726e5ccf4113b63275c1d6c86a0af3e'
 
 FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
 VERIFY_TOKEN = APP_SECRET
-PAGE_ACCESS_TOKEN='EAANjKaTs280BAE8mOHrN4WUKsCByhXkpRxMKprDlSt0wpcPSKV2y954znFBJEiW8NWZAtfvtv2ht1NFjwwoGzw00VEPcQzBesKdqXmOqgM9HG6l1lwnGa1YF1jwcZAhs9J2I3gpbMw0POoNTj8vRF6MWG8IcgNbry6Htq7o3ZAEJkPyQgLxOg13y5fN5XQZD'
+
 
 def verify_webhook(req):
     token = req.args.get('hub.verify_token')
@@ -64,29 +66,6 @@ def verify_webhook(req):
         print('verified')
         return str(challenge)
     return '400'
-
-def send_message(recipient_id, bot_response):
-    """Send a response to Facebook"""
-    payload = {
-        'message': {'text': bot_response},
-        'recipient': {'id': recipient_id},
-        'notification_type': 'regular'
-    }
-
-    auth = {'access_token': PAGE_ACCESS_TOKEN}
-
-    response = requests.post(
-        FB_API_URL,
-        params=auth,
-        json=payload
-    )
-
-    return response.json()
-
-def get_bot_response(message):
-    """This is just a dummy function, returning a variation of what
-    the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
 
 def is_user_message(message):
     """Check if the message is a message from the user"""
@@ -102,21 +81,24 @@ def webhook():
 
     elif request.method == 'POST':
         payload = request.json
-        #print('payload: ', payload)
-        # event = payload['entry'][0]['messaging']
-        # for msg in event:
-        #     if is_user_message(msg):
-        #         text = msg['message']['text']
-        #         temp = {'text': text}
-        #         msg['message'] = temp
-        #         LeadService.add_new_lead(msg)
-        #         bot_response = get_bot_response(text)
-        #         send_message(msg['sender']['id'], bot_response)
+        # print('payload: ', payload)
+        event = payload['entry'][0]['messaging']
+        for msg in event:
+            if is_user_message(msg):
+                text = msg['message']['text']
+                temp = {'text': text}
+                msg['message'] = temp
+                msg['page_id'] = payload['entry'][0]['id']
+                msg['update_time'] = payload['entry'][0]['time']
+                flat_msg = helper.flatten_json(msg)
+                flat_msg['update_time'] = helper.convert_epoch_milliseconds_to_datetime_string(flat_msg['update_time'])
+                flat_msg['timestamp'] = helper.convert_epoch_milliseconds_to_datetime_string(flat_msg['timestamp'])
+                db_obj.insert_row('fb','messages',flat_msg)
 
-        return "Message received"
+        return Response(status=200)
 
     else:
-        return '200'
+        return Response(status=200)
 
 if __name__ == '__main__':
     app.run(debug=True)
