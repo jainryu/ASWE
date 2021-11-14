ebb1f093a6a04e56b8a93a04c92d3dbe732a72b4
Gregory Fu <gregoryfu24@gmail.com>
2021-11-14 14:40:49 -0500

    feat: added user registration and basic authentication

--- server/app.py
+++ server/app.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, jsonify, Response
 from flask_httpauth import HTTPBasicAuth
-# from thumbtack_conn import thumbtack_json_to_pandas
+from passlib.hash import sha256_crypt
+import validators
+#from thumbtack_conn import thumbtack_json_to_pandas
+import json
 import pandas as pd
 import server.db as db
 import server.helper as helper
 from misc.thumbtack_conn import thumbtack_lead_json_to_pandas
+import db
+import helper
+import uuid
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-
-USER_DATA = {
-    "jenna": "smith22"
-}
 
 app.config.from_pyfile('config.py')
 
@@ -20,10 +22,10 @@ db_obj = db.Database(database_url)
 
 
 @auth.verify_password
-def verify(username, password):
-    if not (username and password):
-        return False
-    return USER_DATA.get(username) == password
+def verify_password(username, password):
+    user = json.loads(db_obj.get_data(db_schema='talking_potato', table_name='users', filter_data={'username': username}))
+    if len(user) > 0  and sha256_crypt.verify(password, user[0]['password']):
+        return username
 
 
 @app.route("/")
@@ -37,9 +39,8 @@ def hello_world():
 # TODO: initialize with the right business credentials/api keys
 
 @app.route("/thumbtack_lead", methods=["POST"])
+@auth.login_required
 def receive_lead():
-    if not verify(request.authorization['username'], request.authorization['password']):
-        return {'status': 'bad password'}, 401
     print("data ", request.json)
     data = {"status": "success"}
 
@@ -49,15 +50,53 @@ def receive_lead():
 
 
 @app.route("/thumbtack_messages", methods=["POST"])
+@auth.login_required
 def receive_message():
-    if not verify(request.authorization['username'], request.authorization['password']):
-        return {'status': 'bad password'}, 401
     print("data ", request.json)
     data = {"status": "success"}
 
     # process data and add to database
 
     return data, 200
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.args.get("email")
+    username = request.args.get("username")
+    password = request.args.get("password")
+    email_query = json.loads(db_obj.get_data(db_schema='talking_potato', 
+        table_name='users', filter_data={'email': email}))
+    name_query = json.loads(db_obj.get_data(db_schema='talking_potato', 
+        table_name='users', filter_data={'username': username}))
+    if not validators.email(email):
+        return {'status': 'bad email'}, 400 
+    if len(username) < 3:
+        return {'status': 'username too short'}, 400 
+    if len(password) < 6:
+        return {'status': 'password too short'}, 400 
+    if not username.isalnum():
+        return {'status': 'username must be alphanumeric'}, 400
+    if len(email_query) > 0:
+        return {'status': 'email already registered'}, 400
+    if len(name_query) > 0:
+        return {'status': 'username already registered'}, 400
+        
+    password_hash = sha256_crypt.encrypt(password)
+    entry = {'user_id': str(uuid.uuid4()), 'username': username, 
+        'password': password_hash, 'email': email}
+    if request.args.get('phone_number'):
+        entry['phone_number'] = request.args.get("phone_number")
+    if request.args.get('thumbtack_user_id') and request.args.get('thumbtack_api_key'):
+        entry['thumbtack_user_id'] = request.args.get('thumbtack_user_id')
+        entry['thumbtack_api_key'] = request.args.get('thumbtack_api_key')
+    if request.args.get('facebook_user_id') and request.args.get('fb_app_secret_key') \
+            and request.args.get('fb_page_access_token'):
+        entry['facebook_user_id'] = request.args.get('facebook_user_id')
+        entry['fb_app_secret_key'] = request.args.get('fb_app_secret_key')
+        entry['fb_page_access_token'] = request.args.get('fb_page_access_token')
+    db_obj.insert_row('talking_potato', 'users', entry)
+    return {'status': 'success'}, 200
 
 
 APP_SECRET = '6726e5ccf4113b63275c1d6c86a0af3e'
@@ -130,6 +169,7 @@ def get_messages():
     result = db_obj.get_data(db_schema='talking_potato', table_name='messages', filter_data=filter_data)
     return result
 
+
 @app.route("/get_leads", methods=['GET'])
 def get_leads():
     filter_data = {}
@@ -143,6 +183,7 @@ def get_leads():
         filter_data['date(contacted_time)'] = contacted_date
     result = db_obj.get_data(db_schema='thumbtack', table_name='leads', filter_data=filter_data)
     return result
+
 
 if __name__ == '__main__':
     app.run(debug=True)
