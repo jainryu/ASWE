8b45ae654fcc51e1a85d322b92f72c9a57fa53a4
Animesh Bhasin <animeshbhasin1994@gmail.com>
2021-11-13 19:55:01 -0500

    Added function for inserting row into db

--- server/db.py
+++ server/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 
 
 class Database:
-    def __init__(self):
+    def __init__(self, database_url):
         self.engine = create_engine('postgresql+psycopg2://postgres:postgres@35.238.149.103/tp')
 
     def get_all_leads(self):
@@ -11,3 +11,27 @@ class Database:
         )
 
         return result
+
+    def insert_row(self, db_schema, table_name, create_data):
+        cols = []
+        vals = []
+        args = []
+
+        for k, v in create_data.items():
+            cols.append(k)
+            vals.append('{}')
+            if type(v) == str:
+                args.append("'"+v+"'")
+            else:
+                args.append(v)
+
+        cols_clause = "(" + ",".join(cols) + ")"
+        vals_clause = "values (" + ",".join(vals) + ")"
+
+        sql_stmt = "insert into " + db_schema + "." + table_name + " " + cols_clause + \
+                   " " + vals_clause
+        sql_stmt = sql_stmt.format(*args)
+        self.engine.execute(sql_stmt)
+
+
+
