210e7046b7ae01c6c42a9eb9ee0046e7a2bdb003
Animesh Bhasin <animeshbhasin1994@gmail.com>
2021-11-14 11:02:39 -0500

    Added an endpoint for searching for leads on user_source and contacted_date

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, Response
 from flask_httpauth import HTTPBasicAuth
-#from thumbtack_conn import thumbtack_json_to_pandas
+# from thumbtack_conn import thumbtack_json_to_pandas
 import pandas as pd
 import db
 import helper
@@ -17,11 +17,13 @@ app.config.from_pyfile('config.py')
 database_url = app.config['DATABASE_URL']
 db_obj = db.Database(database_url)
 
+
 @auth.verify_password
 def verify(username, password):
     if not (username and password):
         return False
     return USER_DATA.get(username) == password
+
 
 @app.route("/")
 def hello_world():
@@ -29,23 +31,26 @@ def hello_world():
     df = pd.DataFrame(list(result.fetchall()))
     x = (df.to_json(orient="records"))
     return x
-# TODO: initialize with the right business credentials/api keys 
+
+
+# TODO: initialize with the right business credentials/api keys
 
 @app.route("/thumbtack_lead", methods=["POST"])
 def receive_lead():
     if not verify(request.authorization['username'], request.authorization['password']):
-        return {'status': 'bad password'}, 401 
+        return {'status': 'bad password'}, 401
     print("data ", request.json)
     data = {"status": "success"}
 
     # process data and add to database
 
     return data, 200
+
 
 @app.route("/thumbtack_messages", methods=["POST"])
 def receive_message():
     if not verify(request.authorization['username'], request.authorization['password']):
-        return {'status': 'bad password'}, 401 
+        return {'status': 'bad password'}, 401
     print("data ", request.json)
     data = {"status": "success"}
 
@@ -53,7 +58,8 @@ def receive_message():
 
     return data, 200
 
-APP_SECRET='6726e5ccf4113b63275c1d6c86a0af3e'
+
+APP_SECRET = '6726e5ccf4113b63275c1d6c86a0af3e'
 
 FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
 VERIFY_TOKEN = APP_SECRET
@@ -67,13 +73,15 @@ def verify_webhook(req):
         return str(challenge)
     return '400'
 
+
 def is_user_message(message):
     """Check if the message is a message from the user"""
     return (message.get('message') and
             message['message'].get('text') and
             not message['message'].get("is_echo"))
 
-@app.route("/fb_lead", methods=['GET','POST'])
+
+@app.route("/fb_lead", methods=['GET', 'POST'])
 def webhook():
     print('ok')
     if request.method == 'GET':
@@ -95,14 +103,32 @@ def webhook():
                 flat_msg['update_time'] = helper.convert_epoch_milliseconds_to_datetime_string(flat_msg['update_time'])
                 flat_msg['timestamp'] = helper.convert_epoch_milliseconds_to_datetime_string(flat_msg['timestamp'])
 
-                db_obj.insert_row('fb','messages',flat_msg)
+                db_obj.insert_row('fb', 'messages', flat_msg)
 
         return Response(status=200)
 
     else:
         return Response(status=200)
 
+
+@app.route("/get_messages", methods=['GET'])
+def get_messages():
+    filter_data = {}
+    source = request.args.get('source')
+    contacted_date = request.args.get('date')
+
+    if source:
+        source = source.replace("'", "")
+        filter_data['user_source'] = source
+    if contacted_date:
+        contacted_date = contacted_date.replace("'", "")
+        date_format_check = helper.check_date_format(contacted_date)
+        if not date_format_check:
+            return 'Please enter the date in YYYY-MM-DD format'
+        filter_data['date(contacted_time)'] = contacted_date
+    result = db_obj.get_data(db_schema='talking_potato', table_name='messages', filter_data=filter_data)
+    return result
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
