b250a9ec5e6804eadfade346762fe7761cfe9602
Animesh Bhasin <animeshbhasin1994@gmail.com>
2021-11-15 00:09:20 -0500

    Moved verify_webhook function back to app.py

--- server/fb_helper.py
+++ server/fb_helper.py
@@ -1,18 +1,3 @@
-def verify_webhook(req, username):
-    """webhook verification required by facebook
-
-    :param flask.Request req: the flask request object
-    :return string: either the challenge received by facebook, or http status 400
-    """
-    query = json.loads(db_obj.get_data(db_schema='talking_potato', 
-        table_name='users', filter_data={'username': username}))
-    verify_token = query[0]['fb_app_secret_key']
-    token = req.args.get('hub.verify_token')
-    challenge = req.args.get('hub.challenge')
-    if token == verify_token:
-        print('verified')
-        return str(challenge)
-    return '400'
 
 def is_user_message(message):
     """verify that the message received is text (not image, etc)
