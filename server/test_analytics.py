105758b8ae4a30ed87e5971083228e3dfb5567ff
Animesh Bhasin <animeshbhasin1994@gmail.com>
2021-11-15 16:14:36 -0500

    Style checker compliant

--- server/test_analytics.py
+++ server/test_analytics.py
@@ -1,54 +1,90 @@
+"""
+unit test for analytics module
+"""
+
 import unittest
 from analytics import Analytics
 
 
-class Test_TestAnalytics(unittest.TestCase):
+class TestTestAnalytics(unittest.TestCase):
+    """
+    unit test class for analytics.py
+    """
+
     def setUp(self) -> None:
-        self.a = Analytics('postgresql+psycopg2://postgres:postgres@35.238.149.103/tp')
+        """Setup analytics unit testing table
+
+        :return None
+        """
+        self.a_obj = Analytics('postgresql+psycopg2://postgres:postgres@35.238.149.103/tp')
 
         self.db_schema = 'talking_potato'
         self.table_name = 'analytics_unit_test'
 
-        data = '''{}.{}(user_id varchar, contacted_time timestamp, thumbtack_business_id varchar);''' \
-            .format(self.db_schema, self.table_name)
+        data = f'''{self.db_schema}.{self.table_name} (user_id varchar, contacted_time timestamp,
+        thumbtack_business_id varchar); '''
         create_unit_test_table = 'create table if not exists ' + data
 
-        self.a.engine.execute(create_unit_test_table)
+        self.a_obj.engine.execute(create_unit_test_table)
 
     def tearDown(self):
-        drop_unit_test_table = '''drop table {}.{};'''.format(self.db_schema, self.table_name)
-        self.a.engine.execute(drop_unit_test_table)
-        self.a = None
+        """Tear down analytics unit testing table
+
+        :return None
+        """
+        drop_unit_test_table = f'''drop table {self.db_schema}.{self.table_name};'''
+        self.a_obj.engine.execute(drop_unit_test_table)
+        self.a_obj = None
 
     def test_get_select_clause_cols(self):
+        """Unit test to test get_select_clause_cols method
+
+        :return None
+        """
         expected_clause = 'where user_id={} AND date(contacted_time)={}'
         expected_args = ["'animesh'", "'2021-05-10'"]
         filter_data = {'user_id': 'animesh', 'date(contacted_time)': '2021-05-10'}
 
-        actual_clause, actual_args = self.a.get_where_clause_arg(filter_data)
+        actual_clause, actual_args = self.a_obj.get_where_clause_arg(filter_data)
 
         self.assertEqual(expected_clause, actual_clause)
         self.assertEqual(expected_args, actual_args)
 
     def test_get_user_and_date_between_clause(self):
-        expected_clause = '''where user_id='animesh' AND date(contacted_time) between '2021-01-01' AND '2021-11-15' '''
+        """Unit test to test get_user_and_date_between_clause method
+
+        :return None
+        """
+        expected_clause \
+    = '''where user_id='animesh' AND date(contacted_time) between '2021-01-01' AND '2021-11-15' '''
         filter_data = {'user_id': 'animesh', 'from_date': '2021-01-01', 'to_date': '2021-11-15'}
 
-        actual_clause = self.a.get_user_and_date_between_clause(filter_data)
+        actual_clause = self.a_obj.get_user_and_date_between_clause(filter_data)
 
         self.assertEqual(expected_clause, actual_clause)
 
     def test_get_grouped_by_date(self):
-        expected_res = '[{"date":"2021-11-15T00:00:00.000Z","count":1},{"date":"2021-11-05T00:00:00.000Z","count":1}]'
+        """Unit test to test get_grouped_by_date method
 
-        data = [{'user_id': 'animesh', 'contacted_time': '2021-11-05', 'thumbtack_business_id': '100'},
-                {'user_id': 'animesh', 'contacted_time': '2021-11-15', 'thumbtack_business_id': '100'}]
+        :return None
+        """
+        expected_res = '[{"date":"2021-11-15T00:00:00.000Z","count":1},' \
+                       '{"date":"2021-11-05T00:00:00.000Z","count":1}]'
+
+        data = [{'user_id': 'animesh', 'contacted_time': '2021-11-05',
+                 'thumbtack_business_id': '100'},
+                {'user_id': 'animesh', 'contacted_time': '2021-11-15',
+                 'thumbtack_business_id': '100'}]
         for row in data:
-            self.a.insert_row(self.db_schema, self.table_name, row)
+            self.a_obj.insert_row(self.db_schema, self.table_name, row)
 
         filter_data = {'date(contacted_time)': 1}
-        filter_user_date_range = {'user_id': 'animesh', 'from_date': '2021-01-01', 'to_date': '2021-11-15'}
+        filter_user_date_range = {'user_id': 'animesh', 'from_date': '2021-01-01',
+                                  'to_date': '2021-11-15'}
 
-        actual_result = self.a.get_grouped_by_date(self.db_schema, self.table_name, filter_user_date_range, filter_data)
+        actual_result = self.a_obj.get_grouped_by_date(self.db_schema,
+                                                       self.table_name,
+                                                       filter_user_date_range,
+                                                       filter_data)
 
         self.assertEqual(expected_res, actual_result)
--- server/test_db.py
+++ server/test_db.py
@@ -1,46 +1,74 @@
+"""
+unit test for db module
+"""
 import unittest
 from db import Database
 
 
-class Test_TestDatabase(unittest.TestCase):
-    # schema: unittest
-    # table name: test
-    # column name: name
+class TestTestDatabase(unittest.TestCase):
+    """
+    unit test class for db.py
+    """
     def setUp(self) -> None:
-        self.db = Database('postgresql+psycopg2://postgres:postgres@35.238.149.103/tp')
-        create_unit_test_table = '''create table if not exists talking_potato.unit_test(id varchar, name varchar);'''
-        self.db.engine.execute(create_unit_test_table)
+        """Setup database unit testing table
+
+        :return None
+        """
+        self.db_obj = Database('postgresql+psycopg2://postgres:postgres@35.238.149.103/tp')
+        create_unit_test_table \
+            = '''create table if not exists talking_potato.unit_test(id varchar, name varchar);'''
+        self.db_obj.engine.execute(create_unit_test_table)
         self.db_schema = 'talking_potato'
         self.table_name = 'unit_test'
 
     def tearDown(self):
+        """Tear down database unit testing table
+
+        :return None
+        """
         drop_unit_test_table = '''drop table talking_potato.unit_test;'''
-        self.db.engine.execute(drop_unit_test_table)
-        self.db = None
+        self.db_obj.engine.execute(drop_unit_test_table)
+        self.db_obj = None
 
     def test_get_thumbtack_auth(self):
+        """Unit test to test get_thumbtack_auth method
+
+        :return None
+        """
         expected_result = [('thumbtack_partner', 'thumbt@ck!')]
         business_id = '286845156044809661'
-        self.assertEqual(self.db.get_thumbtack_auth(business_id), expected_result)
+        self.assertEqual(self.db_obj.get_thumbtack_auth(business_id), expected_result)
 
     def test_run_sql(self):
+        """Unit test to test run_sql method
+
+        :return None
+        """
         update_query = '''insert into talking_potato.unit_test values ('test', 'animeshbhasin');'''
-        self.db.run_sql(update_query, commit_flag=True)
+        self.db_obj.run_sql(update_query, commit_flag=True)
         assert True  # To make sure we reach here
 
         expected_result = '''[{"id":"test","name":"animeshbhasin"}]'''
         select_query = '''select * from talking_potato.unit_test ;'''
-        self.assertEqual(self.db.run_sql(select_query, fetch_flag=True), expected_result)
+        self.assertEqual(self.db_obj.run_sql(select_query, fetch_flag=True), expected_result)
 
     def test_insert_row(self):
+        """Unit test to test insert_row method
+
+        :return None
+        """
         create_data = {
             'id': 'test',
             'name': 'animeshbhasin'
         }
-        self.db.insert_row(self.db_schema, self.table_name, create_data)
+        self.db_obj.insert_row(self.db_schema, self.table_name, create_data)
         assert True  # To make sure we reach here
 
     def test_get_where_clause_arg(self):
+        """Unit test to test get_where_clause_arg method
+
+        :return None
+        """
         expected_clause = 'where id={} AND name={}'
         expected_args = ["'test'", "'animeshbhasin'"]
         filter_data = {
@@ -48,18 +76,22 @@ class Test_TestDatabase(unittest.TestCase):
             'name': 'animeshbhasin'
         }
 
-        actual_clause, actual_args = self.db.get_where_clause_arg(filter_data)
+        actual_clause, actual_args = self.db_obj.get_where_clause_arg(filter_data)
 
         self.assertEqual(expected_clause, actual_clause)
         self.assertEqual(expected_args, actual_args)
 
     def test_get_data(self):
+        """Unit test to test get_data method
+
+        :return None
+        """
         expected_result = '''[{"id":"test","name":"animeshbhasin"}]'''
         data = {
             'id': 'test',
             'name': 'animeshbhasin'
         }
-        self.db.insert_row(self.db_schema, self.table_name, data)
-        actual_result = self.db.get_data(self.db_schema, self.table_name, filter_data=data)
+        self.db_obj.insert_row(self.db_schema, self.table_name, data)
+        actual_result = self.db_obj.get_data(self.db_schema, self.table_name, filter_data=data)
 
         self.assertEqual(expected_result, actual_result)
