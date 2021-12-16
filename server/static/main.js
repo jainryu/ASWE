d7b43e69f17fd027bfc1f4059213437671aa57ba
tojoabella <terricabella@gmail.com>
2021-12-15 19:13:14 -1000

    separate registration and edit because of different http request methods

--- server/static/main.js
+++ server/static/main.js
@@ -1,16 +1,31 @@
-let btn = document.getElementById("register_form_submit");
-btn.addEventListener('click', () => {
+let register_btn = document.getElementById("register_form_submit");
+register_btn.addEventListener('click', () => {
     let form = document.getElementById("register_form");
     let formData = new FormData(form);
     let params = new URLSearchParams(formData).toString();
    params = "register?" + params;
-   console.log(params)
 
-    $.post(
-        params,
-        {},
-        function(data) {
-          alert("Response: " + data);
-        }
-    );
+   $.ajax({
+    url: params,
+    type: 'POST',
+    success: function(response) {
+      console.log(response);
+    }
+ });
+});
+
+let edit_btn = document.getElementById("edit_form_submit");
+edit_btn.addEventListener('click', () => {
+    let form = document.getElementById("edit_form");
+    let formData = new FormData(form);
+    let params = new URLSearchParams(formData).toString();
+   params = "register?" + params;
+
+   $.ajax({
+    url: params,
+    type: 'PUT',
+    success: function(response) {
+      console.log(response);
+    }
+    });
 });
